09ba67dc6ea9779edbad8c5f6cadad5cc071d997
Omar Pérez <57925681+omarperezgonzalez@users.noreply.github.com>
2022-03-25 04:18:42 -0600

    Components Files

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,6 @@ const Title = () => {
     <div className="title">
       <h1>Special Thanks to: "Secretaria de Patos del Tec", For providing most of the Pictures</h1>
       <h1>Follow them on Twitter: <a href='https://twitter.com/sptmcm' target='_blank'>@sptmcm</a></h1>
-      <h2>Duck Gallery</h2>
       <p>A special collection of the ducks of Tec Campus MTY</p>
     </div>
   )
